965f6799f95319519a525a8922e39ca455d31925
Khoi Nguyen <dangkhoinguyen1501@gmail.com>
2024-05-10 23:03:23 +0700

    change file

--- javaScript/SearchMethod.js
+++ javaScript/SearchMethod.js
@@ -8,7 +8,7 @@ search_tap.addEventListener("click", function(e) {
 
 document.addEventListener("click", function(e) {
     SearchList.style.display = "none";
-    TapIcon.style.display ="block"
+   
 });
 
 // man hinh nho
@@ -19,7 +19,7 @@ TapIcon.addEventListener("mousemove",function(e){
    
     var modal = document.querySelector("#modal_search")
     var input = document.querySelector("#input")
-    //TapIcon.style.display ="none"
+  
     modal.style.display ="block"
     input.style.display ="block"
     
@@ -36,355 +36,359 @@ TapIcon.addEventListener("mousemove",function(e){
 var mockData =[
 {
     name: 'Quần Jeans Ống Đứng Dài',
-    picture: '/Images/IMG_Product/IMG_Quan/jean/item1/1.jpg',
+    picture: '../img_quanjean/item1_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanJean/item1.html"
 },
 {
     name: 'Quần Jeans Xanh Bạc',
-    picture: '/Images/IMG_Product/IMG_Quan/jean/item2/1.jpg',
+    picture: '../img_quanjean/item2_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanJean/item2.html"
 },
 
 {
     name: 'Quần Jeans Đen Ống Ôm',
-    picture: '/Images/IMG_Product/IMG_Quan/jean/item3/1.jpg',
+    picture: '../img_quanjean/item3_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanJean/item3.html"
 },
 
 {
     name: 'Quần Jeans Đen Ống Rộng',
-    picture: '/Images/IMG_Product/IMG_Quan/jean/item4/1.jpg',
+    picture: '../img_quanjean/item4_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanJean/item4.html"
 },
 
 {
     name: 'Quần Jeans Bạc Thời Trang',
-    picture: '/Images/IMG_Product/IMG_Quan/jean/item4/4.jpg',
+    picture: '../img_quanjean/item4_4.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanJean/item8.html"
 },
 
 {
     name: 'Quần Jeans Ống Rộng Hàn Quốc',
-    picture: '/Images/IMG_Product/IMG_Quan/jean/item5/2.jpg',
+    picture: '../img_quanjean/item5_2.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanJean/item7.html"
 },
 
 {
     name: 'Quần KaKi Xám Thời Trang',
-    picture: '/Images/IMG_Product/IMG_Quan/Kaki/item1/1.jpg',
+    picture: '../img_quankaki/item1_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanKaki/item1.html"
 },
 
 {
     name: 'Quần KaKi Đen Form Rộng',
-    picture: '/Images/IMG_Product/IMG_Quan/Kaki/item2/1.jpg',
+    picture: '../img_quankaki/item2_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanKaki/item2.html"
 },
 
 {
     name: 'Quần KaKi Nâu Cà Phê',
-    picture: '/Images/IMG_Product/IMG_Quan/Kaki/item3/1.jpg',
+    picture: '../img_quankaki/item3_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanKaki/item3.html"
 },
 
 {
     name: 'Quần Kaki Xanh Rêu Dáng Cao',
-    picture: '/Images/IMG_Product/IMG_Quan/Kaki/item4/2.jpg',
+    picture: '../img_quankaki/item4_2.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanKaki/item4.html"
 },
 {
     name: 'Quần Tây Xám Dáng Đứng',
-    picture: '/Images/IMG_Product/IMG_Quan/QuanTay/item3/1.jpg',
+    picture: '../img_quantay/item3_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanTay/item3.html"
 },
 
 {
     name: 'Quần Tây Nâu Sáng Thời Trang',
-    picture: '/Images/IMG_Product/IMG_Quan/QuanTay/item2/1.jpg',
+    picture: '../img_quantay/item2_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanTay/item2.html"
 },
 
 {
     name: 'Quần Tây Xám Đen Công Sở',
-    picture: '/Images/IMG_Product/IMG_Quan/QuanTay/item1/1.jpg',
+    picture: '../img_quantay/item1_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanTay/item1.html"
 },
 
 {
     name: 'Quần Tây Sọc Caro Xanh Đen',
-    picture: '/Images/IMG_Product/IMG_Quan/QuanTay/item4/1.jpg',
+    picture: '../img_quantay/item4_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanTay/item5.html"
 },
 
 {
     name: 'Quần Tây Đen Form Ôm Thời Trang',
-    picture: '/Images/IMG_Product/IMG_Quan/QuanTay/item6/4.jpg',
+    picture: '../img_quantay/item6_4.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanTay/item4.html"
 },
 
 {
     name: 'Quần Short Đen Vải Jean',
-    picture: '/Images/IMG_Product/IMG_Quan/short/item1/1.jpg',
+    picture: '../img_quanshort/item1_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanShort/item1.html"
 },
 
 {
     name: 'Quần Short Sọc Xanh Trắng',
-    picture: '/Images/IMG_Product/IMG_Quan/short/item2/1.jpg',
+    picture: '../img_quanshort/item2_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanShort/item2.html"
 },
 
 {
     name: 'Quần Short Vải Trơn Xanh Lá',
-    picture: '/Images/IMG_Product/IMG_Quan/short/item3/1.jpg',
+    picture: '../img_quanshort/item3_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanShort/item3.html"
 },
 
 {
     name: 'Quần Short Xanh Bạc Vải Jean',
-    picture: '/Images/IMG_Product/IMG_Quan/short/item4/1.jpg',
+    picture: '../img_quanshort/item4_1.jpg',
     link:"/SourceCode/SanPham/Detail/Quan/QuanShort/item4.html"
 },
 
+
+
+
+
 {
     name: 'Áo Khoác Jean Cổ Da',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Jean/Item1/2.webp',
+    picture: '../img_aokhoacjean/item1_2.webp',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoKhoacJean/item1.html"
 },
 {
     name: 'Áo Khoác Jean Vàng Thu',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Jean/Item2/1.webp',
+    picture: '../img_aokhoacjean/item2_1.webp',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoKhoacJean/item2.html"
 },
 {
     name: 'Áo Khoác Dù Đen Có Cổ',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Du/item1/1.webp',
+    picture: '../img_aokhoacdu/item1_1.webp',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoKhoacDu/item1.html"
 },
 {
     name: 'Áo Khoác Dù Xanh Cổ Đứng',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Du/item2/2.webp',
+    picture: '../img_aokhoacdu/item2_1.webp',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoKhoacDu/item2.html"
 },
 {
     name: 'Áo Khoác Dù Xám Vải Lưới',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Du/item3/1.webp',
+    picture: '../img_aokhoacdu/item3_1.webp',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoKhoacDu/item3.html"
 },
 
 {
     name: 'Áo Hoodie Nâu Cafe',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Hoodie/item1/1.jpg',
+    picture: '../img_aohoodie/item1_1.jpg',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoHoodie/item1.html"
 },
 {
     name: 'Áo Hoodie Đen Vua Hải Tặc',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Hoodie/item2/1.jpg',
+    picture: '../img_aohoodie/item2_1.jpg',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoHoodie/item2.html"
 },
 {
     name: 'Áo Hoodie Xanh SkyBlue',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Hoodie/item3/1.jpg',
+    picture: '../img_aohoodie/item3_1.jpg',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoHoodie/item3.html"
 },
 {
     name: 'Áo Hoodie Xanh Navi Thời Trang',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Hoodie/item4/1.jpg',
+    picture: '../img_aohoodie/item4_1.jpg',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoHoodie/item4.html"
 },
 {
     name: 'Áo Khoác Da Lót Dù',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Da/Item1/1.webp',
+    picture: '../img_aokhoacda/item1_1.webp',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoKhoacDa/item1.html"
 },
 {
     name: 'Áo Khoác Da Lót Lông',
-    picture: '/Images/IMG_Product/IMG_AoKhoac/Da/Item2/1.webp',
+    picture: '../img_aokhoacda/item2_1.webp',
     link:"/SourceCode/SanPham/Detail/AoKhoac/AoKhoacDa/item2.html"
 },
 
 {
     name: 'Balo Nữ Dễ Thương',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Balo/item1/1.jpeg',
+    picture: '../img_balo/item1_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Balo/item1.html"
 },
 {
     name: 'Balo Đen Vải Canvas',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Balo/item2/1.jpeg',
+    picture: '../img_balo/item2_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Balo/item2.html"
 },
 {
     name: 'Balo Vải Vân',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Balo/item3/1.jpeg',
+    picture: '../img_balo/item3_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Balo/item3.html"
 },
 {
     name: 'Balo Đen Học Sinh',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Balo/item4/1.png',
+    picture: '../img_balo/item4_1.png',
     link:"/SourceCode/SanPham/Detail/PhuKien/Balo/item4.html"
 },
 {
     name: 'Balo Đen Da Thời Trang',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Balo/item5/1.jpeg',
+    picture: '../img_balo/item5_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Balo/item5.html"
 },
 
 {
     name: 'Dây Nịt Da Cao Cấp',
-    picture: '/Images/IMG_Product/IMG_PhuKien/DayNit/item2/1.jpeg',
+    picture: '../img_daynit/item2_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/DayNit/item2.html"
 },
 {
     name: 'Dây Nịt Nút Gài',
-    picture: '/Images/IMG_Product/IMG_PhuKien/DayNit/item1/1.jpeg',
+    picture: '../img_daynit/item1_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/DayNit/item1.html"
 },
 {
     name: 'Dây Nịt Khóa Platinum',
-    picture: '/Images/IMG_Product/IMG_PhuKien/DayNit/item3/1.jpeg',
+    picture: '../img_daynit/item3_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/DayNit/item3.html"
 },
 {
     name: 'Dây Nịt Khóa Cách Điệu',
-    picture: '/Images/IMG_Product/IMG_PhuKien/DayNit/item4/1.jpeg',
+    picture: '../img_daynit/item4_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/DayNit/item4.html"
 },
 {
     name: 'Dây Nịt Nam Thời Trang',
-    picture: '/Images/IMG_Product/IMG_PhuKien/DayNit/item5/1.jpeg',
+    picture: '../img_daynit/item5_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/DayNit/item5.html"
 },
 
 {
     name: 'Dép Cười',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Dep/item1/1.jpeg',
+    picture: '../img_dep/item1_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Dep/item1.html"
 },
 {
     name: 'Dép DUWA Đi Phố',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Dep/item2/1.jpeg',
+    picture: '../img_dep/item2_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Dep/item2.html"
 },
 {
     name: 'Dép Bánh Mì',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Dep/item3/1.jpeg',
+    picture: '../img_dep/item3_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Dep/item3.html"
 },
 {
     name: 'Dép Sandal Đi Học',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Dep/item4/1.jpeg',
+    picture: '../img_dep/item4_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Dep/item4.html"
 },
 
 {
     name: 'Giày Adidas Trắng',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Giay/item1/1.jpeg',
+    picture: '../img_giay/item1_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Giay/item1.html"
 },
 {
     name: 'Giày Sneaker Đi Phố',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Giay/item2/1.jpeg',
+    picture: '../img_giay/item2_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Giay/item2.html"
 },
 {
     name: 'Giày Tăng Chiều Cao',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Giay/item3/1.jpeg',
+    picture: '../img_giay/item3_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Giay/item3.html"
 },
 
 {
     name: 'Nón Lưỡi Trai Thời Trang',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Non/item1/1.jpeg',
+    picture: '../img_non/item1_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Non/item1.html"
 },
 {
     name: 'Nón Tai Bèo',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Non/item2/2.jpeg',
+    picture: '../img_non/item2_2.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Non/item2.html"
 },
 {
     name: 'Nón Nồi Cam',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Non/item3/1.jpeg',
+    picture: '../img_non/item3_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Non/item3.html"
 },
 
 {
     name: 'Vớ Họa Tiết',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Vo/item1/1.jpeg',
+    picture: '../img_vo/item1_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Vo/item1.html"
 },
 {
     name: 'Vớ Ngang Cổ Chân',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Vo/item2/1.jpeg',
+    picture: '../img_vo/item2_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Vo/item2.html"
 },
 {
     name: 'Vớ Dài Trơn',
-    picture: '/Images/IMG_Product/IMG_PhuKien/Vo/item3/1.jpeg',
+    picture: '../img_vo/item3_1.jpeg',
     link:"/SourceCode/SanPham/Detail/PhuKien/Vo/item3.html"
 },
 
 {
     name: 'Áo Polo Oversize',
-    picture: '/Images/IMG_Product/IMG_Ao/Polo/item1/1.jpeg',
+    picture: '../img_polo/item1_1.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoPolo/item1.html"
 },
 {
     name: 'Áo Polo Khóa Kéo',
-    picture: '/Images/IMG_Product/IMG_Ao/Polo/item2/1.jpeg',
+    picture: '../img_polo/item2_1.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoPolo/item2.html"
 },
 {
     name: 'Áo Polo Vải Nỉ',
-    picture: '/Images/IMG_Product/IMG_Ao/Polo/item3/1.jpeg',
+    picture: '../img_polo/item3_1.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoPolo/item3.html"
 },
 
 {
     name: 'Áo Sơ Mi Vải Nỉ',
-    picture: '/Images/IMG_Product/IMG_Ao/SoMi/item1/1.jpeg',
+    picture: '../img_somi/item1_1.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoSoMi/item1.html"
 },
 {
     name: 'Áo Sơ Mi Form Rộng',
-    picture: '/Images/IMG_Product/IMG_Ao/SoMi/item2/3.jpeg',
+    picture: '../img_somi/item2_3.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoSoMi/item2.html"
 },
 {
     name: 'Áo Sơ Mi 3 Sọc',
-    picture: '/Images/IMG_Product/IMG_Ao/SoMi/item3/4.jpeg',
+    picture: '../img_somi/item3_4.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoSoMi/item3.html"
 },
 
 {
     name: 'Áo TankTop City Cycle',
-    picture: '/Images/IMG_Product/IMG_Ao/TankTop/item1/1.jpeg',
+    picture: '../img_tanktop/item1_1.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoTankTop/item1.html"
 },
 {
     name: 'Áo TankTop Trơn',
-    picture: '/Images/IMG_Product/IMG_Ao/TankTop/item2/2.jpeg',
+    picture: '../img_tanktop/item2_2.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoTankTop/item2.html"
 },
 {
     name: 'Áo TankTop Form Rộng',
-    picture: '/Images/IMG_Product/IMG_Ao/TankTop/item3/1.jpeg',
+    picture: '../img_tanktop/item3_1.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoTankTop/item3.html"
 },
 
 {
     name: 'Áo Thun Vải Dày Dặn',
-    picture: '/Images/IMG_Product/IMG_Ao/Thun/item1/1.jpeg',
+    picture: '../img_aothun/item1_1.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoThun/item1.html"
 },
 {
     name: 'Áo Thun Oversize',
-    picture: '/Images/IMG_Product/IMG_Ao/Thun/item2/4.jpeg',
+    picture: '../img_aothun/item2_4.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoThun/item2.html"
 },
 {
     name: 'Áo Thun Cartoon',
-    picture: '/Images/IMG_Product/IMG_Ao/Thun/item3/1.jpeg',
+    picture: '../img_aothun/item3_1.jpeg',
     link:"/SourceCode/SanPham/Detail/Ao/AoThun/item3.html"
 },
 
